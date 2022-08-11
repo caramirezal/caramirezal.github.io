4671039fba8873684ae2e898df45fe8798e984ca
caramirezal <cramireza@ciencias.unam.mx>
2022-08-11 16:34:02 +0200

    variable types

---
+++ courses/RCourse/01_00_tipos_de_variables.R
@@ -0,0 +1,23 @@
+## Esto es un comentario y no se ejecuta por R
+
+a <- 3.1416
+print(a)
+
+a = 3.1416
+a     ## Note that we don't need to use print(a)
+
+a <- 100*( 3.1416 + 0.999 ) / 3
+a
+
+b <- ( a**2 ) + 3*a
+b
+
+TRUE & FALSE
+
+A <- TRUE | FALSE
+
+cadena <- 'Trópico para qué me diste las manos de sol'
+cadena
+
+var1 <- 1
+#vaR1s ## This will rise an error
